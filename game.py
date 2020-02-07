b897a1602cfbceb7e86a59b9d7a0065e6b35a6e4
Kevdeme <57798342+Kevdeme@users.noreply.github.com>
2020-02-07 10:56:36 +0100

    New Score system, things now are cars, correction of comments.

--- game.py
+++ game.py
@@ -9,11 +9,13 @@ pygame.init()
 carIMG = pygame.image.load('media/car.png')  # Load Car.
 carIMG = pygame.transform.scale(carIMG, (120, 320))  # Resize Car.
 
+carIMG_2 = pygame.image.load('media/car_2.png')
+
+
 pygame.display.set_icon(carIMG) # Game Icon
 
 # Sounds:
 crash_sound = pygame.mixer.Sound('media/car-crash.ogg')
-
 pygame.mixer.music.load('media/song.ogg')
 
 # Size of the Window
@@ -23,7 +25,7 @@ display_width = 800
 display_height = 1000
 
 #Dinamic Screen Size
-#infoObject = pygame.display.Info()
+infoObject = pygame.display.Info()
 #display_width = infoObject.current_w
 #display_height = infoObject.current_h
 
@@ -38,6 +40,10 @@ grey = (192,192,192)
 bright_red = (255,0,0)
 bright_green = (0,255,0)
 bright_blue = (0,0,255)
+
+dark_green = (0,100,00)
+
+background=white
 
 
 
@@ -59,10 +65,16 @@ def score(count,text,x,y):
     gameDisplay.blit(text,(x,y))
 
 
+def cars(thingx, thingy, car_type):
+    # Creates cars obstacles.
+    #pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+    gameDisplay.blit(car_type, (thingx, thingy))
+
 
 def things(thingx, thingy, thingw, thingh, color):
-    # Creates things with the the arguments
+    # Draw objects with the the arguments
     pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
 
 
 def car(x, y):
@@ -119,6 +131,7 @@ def crash():
         clock.tick(15)
 
 def button(msg,x,y,w,h,color1,color2,action=None):
+    # Reads mouse position/click, draw rectangle w/ arg and execute actions if mouse is over or click the rectangle
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
@@ -145,7 +158,7 @@ def unpause():
 
 
 def paused():
-
+    # Pause music, generate pause screen with text and buttons to return or quit.
     pygame.mixer.music.pause()
 
     global pause
@@ -172,6 +185,7 @@ def paused():
         clock.tick(15)
 
 def game_intro():
+    # Generates Intro screen with GO! and QUIT buttons.
 
     intro = True
 
@@ -186,15 +200,20 @@ def game_intro():
         TextRect.center = ((display_width/2),(display_width/3))
         gameDisplay.blit(TextSurf, TextRect)
 
-        button("GO!", 250, 600, 100, 50, green, bright_green,game_loop)
-        button("QUIT", 450, 600, 100, 50, red, bright_red,quitgame)
+        button("GO!", 250, display_height/1.66, 100, 50, green, bright_green,game_loop)
+        button("QUIT", 450, display_height/1.66, 100, 50, red, bright_red,quitgame)
 
 
         pygame.display.update()
         clock.tick(15)
 
 def game_loop():
+
     global pause
+
+    dodged = 0
+    scores = 0
+
     # Start the music
     pygame.mixer.music.play(-1) # Attribute indicates the amount of time the song is going to play
     music = True
@@ -204,7 +223,6 @@ def game_loop():
     y = (display_height * 0.7)
 
     # Inside the road
-
     inside_l = round(display_width / 6)
     inside_r = round(display_width/1.5)
 
@@ -212,51 +230,62 @@ def game_loop():
     # Initialization of variable for moving the car.
     x_change = 0
 
+    # Parameters for the street lines
     lines_startx = display_width / 2
     lines_starty = -1
     lines_width = 15
     lines_height = 80
 
+    # Exact position for the 3 lanes
     lanes = [display_width / 4.7, display_width / 2.2, display_width / 1.47]
 
+
+    # Parameters for Road cars
     thing_startx = random.choice(lanes)
-    #thing_startx = random.randrange(inside_l, inside_r)
     thing_starty = -700
     thing_speed = 7
-    thing_width = 100
-    thing_height = 100
+    thing_width = 96
+    thing_height = 192
 
-    dodged = 0
-    scores = 0
+    thing2_startx = random.choice(lanes)
+    thing2_starty = random.randrange(-700,0)
 
-    gameExit = False
 
-    while not gameExit:  # Logic Loop of Events.
+    level1 = True
+
+    #gameExit
+
+    while level1:  # Logic Loop of Events.
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:           # If you press the window X.
+            # If you press the window X.
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN:        # If there is a Key being press.
-
-                if event.key == pygame.K_LEFT:      # Side Movement
-                    x_change = -20
+            if event.type == pygame.KEYDOWN:
+                # If there is a Key being press.
+                if event.key == pygame.K_LEFT:
+                    # Side Movement
+                    x_change = -12
                 if event.key == pygame.K_RIGHT:
-                    x_change = +20
+                    x_change = +12
 
-                if event.key == pygame.K_UP:        # Acceleration
+                if event.key == pygame.K_UP:
+                    # Acceleration
                     thing_speed = +30
                     scores += 2
 
-                if event.key == pygame.K_DOWN:      # Brake
+                if event.key == pygame.K_DOWN:
+                    # Brake
                     thing_speed = 5
 
+                if event.key == pygame.K_p:
+                    # Pause
+                    paused()
 
-                if event.key == pygame.K_p:         # Pause
-                        paused()
-
-                if event.key == pygame.K_m:         # Music
+                if event.key == pygame.K_m:
+                    # Music on/off
                     if music == True:
                         pygame.mixer.music.pause()
                         music = False
@@ -266,6 +295,7 @@ def game_loop():
 
 
             if event.type == pygame.KEYUP:
+                # If key is release.
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
 
@@ -274,35 +304,39 @@ def game_loop():
 
         x += x_change
 
-        gameDisplay.fill(green)     # Set background color
+        gameDisplay.fill(dark_green)
+        # Set background color
 
-        pygame.draw.rect(gameDisplay, grey, (inside_l, 0, inside_r, display_height)) #road
-        pygame.draw.rect(gameDisplay, black, (inside_l-30, 0, 20, display_height))   #left guardraid
-        pygame.draw.rect(gameDisplay, black, ((inside_l+inside_r) + 10, 0, 20, display_height))# rigth Guard
+        # Road, Left and Right guardrail.
+        pygame.draw.rect(gameDisplay, grey, (inside_l, 0, inside_r, display_height))
+        pygame.draw.rect(gameDisplay, black, (inside_l-30, 0, 20, display_height))
+        pygame.draw.rect(gameDisplay, black, ((inside_l+inside_r) + 10, 0, 20, display_height))
+
 
         #Street Lines
-
         for i in range(1,2000,150):
             things(lines_startx-100,(lines_starty-display_height)+i,lines_width,lines_height,white)
 
         for i in range(1,2000,150):
             things(lines_startx+100,(lines_starty-display_height)+i,lines_width,lines_height,white)
-        lines_starty += thing_speed
+        lines_starty += thing_speed+10
 
         # Respawn the lines
         if lines_starty > display_height:
             lines_starty = 0 - lines_height/2
 
 
-        # Road Objects
+        # Road Cars
+        cars(thing2_startx, thing2_starty, carIMG_2)
+        thing2_starty += thing_speed
 
-        things(thing_startx, thing_starty, thing_width, thing_height, black)
-        thing_starty += thing_speed
-
+        # More Cars
+        #if dodged > 5:
+        #    things(thing2_startx, thing2_starty,carIMG_2)
+        #    thing2_starty += thing_speed
 
         # Car Initialization
         car(x, y)
-
 
         # Scores
         scores += 1
@@ -313,32 +347,49 @@ def game_loop():
         else:
             scores += 1
 
+        # Score Visualization
         score(dodged,"Dodged: ",0,0)
         score(scores, "Score: ",0,50)
 
 
 
-        if x > display_width - car_width or x < 0: # If the car goes out of the screen
-            crash()                 # Car crashes
+        if x > display_width - car_width or x < 0:
+            # If the car goes out of the screen
+            crash()
 
-
-
-        if thing_starty > display_height:           # If the object is out of screen aka there is no object
-            thing_starty = 0 - thing_height
-            thing_startx = random.choice(lanes) # random.randrange(inside_l, inside_r)   # Area where the objects are going to appear
+        if thing2_starty > display_height:
+            # If the object is out of screen aka there is no object
+            thing2_starty = random.randrange(-700,0) - thing_height
+            thing2_startx = random.choice(lanes)
             dodged += 1
-            #thing_speed += 1                                    # Things moves quicker each time you dodge
-            #thing_width += (dodged * 1.2)                        # Make things bigger each time you dodge
+
+        if thing_starty > display_height:
+            # If the object is out of screen aka there is no object
+            thing_starty = 0 - thing_height
+            thing_startx = random.choice(lanes)
+            # random.randrange(inside_l, inside_r)   # Area where the objects are going to appear
+            dodged += 1
+            #thing_speed += 1 # Things moves quicker each time you dodge
+            #thing_width += (dodged * 1.2) # Make things bigger each time you dodge
 
         if y < thing_starty+thing_height:
-            print('Y Crossover')
+            #print('Y Crossover')
 
-            if (x > thing_startx) and (x < (thing_startx + thing_width)) or ((x + car_width) > thing_startx) and ((x + car_width) < (thing_startx + thing_width)):
-                print('X Crossover')
+            if (x > thing_startx) and (x < (thing_startx + thing_width)) or ((x + car_width) > thing_startx) and \
+                    ((x + car_width) < (thing_startx + thing_width)):
+                #print('X Crossover')
                 crash()
 
-        pygame.display.update()     # Updates screen
-        clock.tick(120)             # FPS
+        if y < thing2_starty + thing_height:
+
+            if (x > thing2_startx) and (x < (thing2_startx + thing_width)) or ((x + car_width) > thing2_startx) \
+                    and ((x + car_width) < (thing2_startx + thing_width)):
+                crash()
+
+        # Update Screen and FPS
+        pygame.display.update()
+        clock.tick(120)
+
         #print(f"Car start at position is {x} and car finish at position {x+car_width}")
         #print(f"object start at position is {thing_startx} and car finish at position {thing_startx+thing_width}")
 
